ddc37e2004dd6aa13e550dea4d888bb94d73943d
monitoo <melisa.a.flores0807@gmail.com>
2026-08-07 15:15:09 -0300

    Add files via upload

---
+++ cudi_frontend1/cudi_frontend1/Form3.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace cudi_frontend1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            comboBox1 = new ComboBox();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label6 = new Label();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.HotTrack;
+            panel1.Controls.Add(label6);
+            panel1.Location = new Point(2, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(798, 50);
+            panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.HotTrack;
+            panel2.Location = new Point(2, 404);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(798, 50);
+            panel2.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.DodgerBlue;
+            panel3.Controls.Add(label5);
+            panel3.Controls.Add(comboBox1);
+            panel3.Controls.Add(textBox4);
+            panel3.Controls.Add(textBox3);
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(textBox1);
+            panel3.Controls.Add(label4);
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(label2);
+            panel3.Controls.Add(label1);
+            panel3.Location = new Point(226, 97);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(344, 227);
+            panel3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(28, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(28, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Apellido:";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(28, 97);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Usuario:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(28, 139);
+            label4.Name = "label4";
+            label4.Size = new Size(118, 25);
+            label4.TabIndex = 3;
+            label4.Text = "Contraseña:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(191, 26);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(128, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(191, 63);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(128, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(191, 102);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(128, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(191, 139);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(128, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Administrador", "Recepcionista" });
+            comboBox1.Location = new Point(191, 183);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(128, 23);
+            comboBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(28, 178);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 25);
+            label5.TabIndex = 9;
+            label5.Text = "Rol:";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Blue;
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(175, 345);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 40);
+            button1.TabIndex = 9;
+            button1.Text = "Guardar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Blue;
+            button2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(330, 345);
+            button2.Name = "button2";
+            button2.Size = new Size(123, 40);
+            button2.TabIndex = 10;
+            button2.Text = "Modificar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Blue;
+            button3.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(488, 345);
+            button3.Name = "button3";
+            button3.Size = new Size(121, 40);
+            button3.TabIndex = 10;
+            button3.Text = "Desactivar";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.ButtonHighlight;
+            label6.Location = new Point(354, 8);
+            label6.Name = "label6";
+            label6.Size = new Size(97, 30);
+            label6.TabIndex = 0;
+            label6.Text = "Usuarios";
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(panel3);
+            Controls.Add(button2);
+            Controls.Add(panel2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Name = "Form3";
+            Text = "Form3";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Label label2;
+        private Label label1;
+        private Label label4;
+        private Label label3;
+        private Label label5;
+        private ComboBox comboBox1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+        private Label label6;
+    }
+}
