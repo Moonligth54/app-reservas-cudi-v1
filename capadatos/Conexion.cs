6410f420c5784fbb8553b4b716da56fa5b15c91b
ThomasCode17 <thomasfeliperiossalazar17@gmail.com>
2026-08-12 13:17:17 -0300

    Add files via upload

---
+++ capadatos/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace capadatos
+{
+    public class Conexion
+    {
+        public static string cadena = ConfigurationManager.ConnectionStrings["cadena_conexion"].ConnectionString;
+    }
+}
